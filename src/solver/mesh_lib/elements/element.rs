6aab66b1df00169a97a97a3269825b1787c1715a
Quentin Huss <quentinhuss@hotmail.com>
2026-01-05 22:22:47 +0100

    updatet C3D4 shape function

--- src/solver/mesh_lib/elements/element.rs
+++ src/solver/mesh_lib/elements/element.rs
@@ -158,20 +158,31 @@ impl Element {
         }
     }
 }
-/// linear tetraeder:
-/// N1 = xi, N2 = eta, N3 = zeta, N4 = 1 - xi - eta - zeta
-/// TODO: Check Node order in Calculix
-/// Node 1=(1,0,0), 2=(0,1,0), 3=(0,0,1), 4=(0,0,0))
-fn shape_func_c3d4(xi: f64, eta: f64, zeta: f64) -> (Vec<f64>, Array2<f64>) {
-    // Formfunktionen
-    let n = vec![xi, eta, zeta, 1.0 - xi - eta - zeta];
 
-    // Ableitungen (konstant für lineares Element)
-    // Zeile 0: d/dxi, Zeile 1: d/deta, Zeile 2: d/dzeta
+/// linear tetraeder
+fn shape_func_c3d4(xi: f64, eta: f64, zeta: f64) -> (Vec<f64>, Array2<f64>) {
+    // Shape functions (CalculiX Convention: Node 1 is Origin)
+    // N1 = 1 - xi - eta - zeta
+    // N2 = xi
+    // N3 = eta
+    // N4 = zeta
+    let n = vec![
+        1.0 - xi - eta - zeta, // Node 1
+        xi,                    // Node 2
+        eta,                   // Node 3
+        zeta,                  // Node 4
+    ];
+
+    // Derivatives
+    // Row 0: d/dxi
+    // Row 1: d/deta
+    // Row 2: d/dzeta
+    //
+    // cols are nodes 1, 2, 3, 4
     let dn = array![
-        [1.0, 0.0, 0.0, -1.0],
-        [0.0, 1.0, 0.0, -1.0],
-        [0.0, 0.0, 1.0, -1.0]
+        [-1.0, 1.0, 0.0, 0.0], // d/dxi:  N1=-1, N2=1
+        [-1.0, 0.0, 1.0, 0.0], // d/deta: N1=-1, N3=1
+        [-1.0, 0.0, 0.0, 1.0]  // d/dzeta: N1=-1, N4=1
     ];
 
     (n, dn)
